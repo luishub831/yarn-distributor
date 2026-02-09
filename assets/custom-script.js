14797729b44ee1bcb1ac368cc2323b44a3b98549
luishub831 <work.group831@gmail.com>
2026-02-09 12:46:47 +0900

    back to RRP

--- assets/custom-script.js
+++ assets/custom-script.js
@@ -64,16 +64,12 @@ function formatPrices() {
 
             let fromText = '';
             if (priceText.toLowerCase().startsWith('from ')) {
-                fromText = 'As low as ';
-                priceText = priceText.substring(10).trim();
+                fromText = 'From ';
+                priceText = priceText.substring(5).trim();
             }
             if (priceText.toLowerCase().startsWith('rrp ')) {
                 fromText = 'RRP ';
                 priceText = priceText.substring(4).trim();
-            }
-            if (priceText.toLowerCase().startsWith('as low as ')) {
-                fromText = 'As low as ';
-                priceText = priceText.substring(10).trim();
             }
 
             let priceMatch = priceText.match(/\$?(\d+(?:\.\d{1,2})?)/);
